fbba9a15d1dca0a629c53a7019b86f5ecd68fba3
Felipe Barrios <abarriosa1@ucentral.edu.co>
2021-09-04 23:27:11 -0500

    BPA

--- MiProyecto.py
+++ MiProyecto.py
@@ -1,8 +1,13 @@
 import numpy as np
+import copy as cpy
 
-estadoInicial = [[3,2,8],
-                 [6,5,1],
-                 [4,7,'□']]
+estadoInicial = [[1,2,3],
+                 [4,5,6],
+                 [7,'□',8]]
+
+estadoFinal = [[1,2,3],
+               [4,5,6],
+               [7,8,'□']]
 
 class Estado:
     def __init__(self,estadoActual,isRaiz,estadoPadre,reglaUsada):
@@ -20,63 +25,89 @@ class Procesamiento:
     def __init__(self):
         pass
 
-    def movIzqBlco(self,estadoActual,posBlanco):
+    def movIzqBlco(self,t,posBlanco):
+        x = cpy.deepcopy(t)
         fila = int(str(posBlanco[0])[1])
         col = int(str(posBlanco[1])[1])
         try:
             if(not col-1 < 0):
-                pivote = estadoActual[fila][col-1]
-                estadoActual[fila][col-1] = '□'
-                estadoActual[fila][col] = pivote
+                pivote = x[fila][col-1]
+                x[fila][col-1] = '□'
+                x[fila][col] = pivote
+                return x
+            else:
+                return None
         except:
             pass
-        print(np.array(estadoActual))
 
-    def movDerBlco(self,estadoActual,posBlanco):
+    def movDerBlco(self,t,posBlanco):
+        x = cpy.deepcopy(t)
         fila = int(str(posBlanco[0])[1])
         col = int(str(posBlanco[1])[1])
         try:
             if(not col+1 > 2):
-                pivote = estadoActual[fila][col+1]
-                estadoActual[fila][col+1] = '□'
-                estadoActual[fila][col] = pivote
+                pivote = x[fila][col+1]
+                x[fila][col+1] = '□'
+                x[fila][col] = pivote
+                return x
+            else:
+                return None
         except:
             pass
-        print(np.array(estadoActual))
     
-    def movArrBlco(self,estadoActual,posBlanco):
+    def movArrBlco(self,t,posBlanco):
+        x = cpy.deepcopy(t)
         fila = int(str(posBlanco[0])[1])
         col = int(str(posBlanco[1])[1])
         try:
             if(not fila-1 < 0):
-                pivote = estadoActual[fila-1][col]
-                estadoActual[fila-1][col] = '□'
-                estadoActual[fila][col] = pivote
+                pivote = x[fila-1][col]
+                x[fila-1][col] = '□'
+                x[fila][col] = pivote
+                return x
+            else:
+                return None
         except:
             pass
-        print(np.array(estadoActual))
 
-    def movAbjBlco(self,estadoActual,posBlanco):
+    def movAbjBlco(self,t,posBlanco):
+        x = cpy.deepcopy(t)
         fila = int(str(posBlanco[0])[1])
         col = int(str(posBlanco[1])[1])
         try:
             if(not fila+1 > 2):
-                pivote = estadoActual[fila+1][col]
-                estadoActual[fila+1][col] = '□'
-                estadoActual[fila][col] = pivote
+                pivote = x[fila+1][col]
+                x[fila+1][col] = '□'
+                x[fila][col] = pivote
+                return x
+            else:
+                return None
         except:
             pass
-        print(np.array(estadoActual))
-
-
-    def procesarRegla(estadoActual,regla):
-        print('procesarRegla')
-
+                
+    def BPA(self,estadoActual,estadosRecorridos):
+        print(estadoActual)
+        matriz = np.array(estadoActual)
+        pos = np.where(matriz == '□')
+        if(estadoActual == None): 
+                return None
+        try:
+            estadosRecorridos.index(estadoActual)
+            return None
+        except:
+            estadosRecorridos.append(estadoActual)
+            if(estadoActual == estadoFinal):
+                return estadoActual
+            else:
+                self.BPA(self.movDerBlco(estadoActual,pos),estadosRecorridos)
+                self.BPA(self.movIzqBlco(estadoActual,pos),estadosRecorridos)
+                self.BPA(self.movArrBlco(estadoActual,pos),estadosRecorridos)
+                self.BPA(self.movAbjBlco(estadoActual,pos),estadosRecorridos)
+            
 def main():
-    matriz = np.array(estadoInicial)
-    pos = np.where(matriz == '□')
-    proc = Procesamiento()
     
+    proc = Procesamiento()
+    proc.BPA(estadoInicial,[])
     
 if __name__ == "__main__":
     main()
